80ce9c6093cbe3dba1b377127eab60c845b2db9d
jessiicamaru <Dandrew2407@gmail.com>
2023-09-20 14:15:32 +0700

    Chapter02_Lab_Exercise6

---
+++ Chapter02_Lab_Exercise6/JavaProject/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        Rectangle rec1 = new Rectangle(23, 20);
+        Rectangle rec2 = new Rectangle(40, 50);
+
+        System.out.println("Area of rectangle 1 is: " + rec1.area());
+        System.out.println("Area of rectangle 2 is: " + rec2.area());
+    }
+}
