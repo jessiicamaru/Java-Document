8d25ffd8d461e31c13ebe0bb64c0ea60e7338e60
jessiicamaru <Dandrew2407@gmail.com>
2023-09-29 08:47:06 +0700

    update enum

--- Document/Enum/src/App.java
+++ Document/Enum/src/App.java
@@ -1,38 +1,73 @@
+import java.util.*;
+
 interface Shape {
-    String calculatorArea();
+    double calculateArea();
 }
 
 enum ShapeType implements Shape {
-    CIRCLE,
-    SQUARE,
-    TRIANGLE;
+    CIRCLE {
+        private int radius;
 
-    private final int sides;
-
-    ShapeType(int sides) {
-        this.sides = sides;
-    }
-
-    @Override
-    public String calculatorArea() {
-        switch (this) {
-            case CIRCLE:
-                return "Circle " + this;
-            case SQUARE:
-                return "Square " + this;
-            case TRIANGLE:
-                return "Triangle " + this;
-            default:
-                return "false";
+        public void setValue(int radius) {
+            this.radius = radius;
         }
-    }
+
+        @Override
+        public double calculateArea() {
+            return Math.PI * radius * radius;
+        }
+    },
+    SQUARE {
+        private int side;
+
+        public void setValue(int side) {
+            this.side = side;
+        }
+
+        @Override
+        public double calculateArea() {
+            return side * side;
+        }
+    },
+    RECTANGLE {
+        private int height, width;
+
+        public void setValue(int height, int width) {
+            this.height = height;
+            this.width = width;
+        }
+
+        @Override
+        public double calculateArea() {
+            return height * width;
+        }
+    },
 }
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-        for (ShapeType shapeType : ShapeType.values()) {
-            System.out.println(shapeType + " is: " + shapeType.calculatorArea());
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input your type of shape: ");
+        String shape = scanner.nextLine();
+
+        shape = shape.toUpperCase();
+
+        switch (shape) {
+            case "CIRCLE":
+                System.out.print("Input radius: ");
+                int radius = scanner.nextInt();
+
+                ShapeType circle = ShapeType.CIRCLE;
+                circle.setValue(radius);
+                break;
+            case "SQUARE":
+                break;
+            case "RECTANGLE":
+                break;
         }
+
+        scanner.close();
     }
 }
